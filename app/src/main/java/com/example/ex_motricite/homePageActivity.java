1c08b4ec2cc7ed646ddaba5079d12c94cb0c91b0
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2023-12-01 11:51:30 +0100

    Les 3 boutons sont maintenant cliquable, il suffira juste de créer les activitys conrrespondantes et de bien les mettre dans l'activity de la page principale.

--- app/src/main/java/com/example/ex_motricite/homePageActivity.java
+++ app/src/main/java/com/example/ex_motricite/homePageActivity.java
@@ -2,13 +2,59 @@ package com.example.ex_motricite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class homePageActivity extends AppCompatActivity {
 
+    private LinearLayout layout_static;
+    private LinearLayout layout_rythm;
+    private LinearLayout layout_patient;
+    private ImageView iv_settings;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+
+        layout_static = findViewById(R.id.layout_static);
+        layout_rythm = findViewById(R.id.layout_rythm);
+        layout_patient = findViewById(R.id.layout_patient);
+        iv_settings =  findViewById(R.id.iv_settings);
+
+        layout_static.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homePageActivity.this, staticActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        layout_rythm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homePageActivity.this, rythmActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        layout_patient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homePageActivity.this, patientActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        iv_settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(homePageActivity.this, settingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
