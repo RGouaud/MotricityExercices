c6ae9c0eb557e2242212088812912572efd37dfe
rgouaud <r.gouaud@gmail.com>
2023-12-04 17:45:11 +0100

    debut de la création de l'écran settings

--- app/src/main/java/com/example/ex_motricite/ExercisesSettingsActivity.java
+++ app/src/main/java/com/example/ex_motricite/ExercisesSettingsActivity.java
@@ -11,4 +11,21 @@ public class ExercisesSettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises_settings);
     }
+
+    // logique
+    /*
+    passage d'un parametre a l'instanciation de la classe pour afficher ou non les parametres relatifs au test dynamiques
+
+    alimentation des spinners avec les données relatives
+
+    SI DYNAMIQUE
+    mise en place de la synchro entre les choix d'interval
+
+
+
+    BOUTON START : jsp
+
+    BOUTON RECOVER : grisé si pas de CSV trouvé  à l'emplacement
+    sinon : lire les méta données du dernier CSV et les écrire dans leurs emplacements
+     */
 }
