b5a8413977b439a112fe84a13fe0f123b2fb4278
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2023-12-05 16:54:26 +0100

    Mise en forme de la page des settings, mise en place du header

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.ex_motricite"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.ex_motricite"
         minSdk = 23
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
