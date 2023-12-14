507473eef116737cd7a5b6e9d70bc27f1111ac67
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2023-12-14 12:28:28 +0100

    Création de la classe de base deonnées et donc des tables operator et patient, début de création des classe dao patient et operator

---
+++ app/src/main/java/com/example/ex_motricite/BdSQLiteOpenHelper.java
@@ -0,0 +1,44 @@
+package com.example.ex_motricite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class BdSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    private String table_patient="create table patient ("
+            + "idPatient INTEGER PRIMARY KEY AUTOINCREMENT,"
+            +"Name TEXT NOT NULL,"
+            +"firstName TEXT NOT NULL,"
+            +"birthDate TEXT NOT NULL,"
+            +"remarques TEXT NOT NULL);";
+
+    private String table_operator="create table operator ("
+            + "idOperator INTEGER PRIMARY KEY AUTOINCREMENT,"
+            +"Name TEXT NOT NULL,"
+            +"firstName TEXT NOT NULL);";
+
+
+    public BdSQLiteOpenHelper(Context context, String name, CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        // TODO Auto-generated constructor stub
+    }
+
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(table_patient);
+        db.execSQL(table_operator);
+
+        db.execSQL("insert into operator (Name, firstName) values('Hentrics', 'Samuel')");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+    }
---
+++ app/src/main/java/com/example/ex_motricite/OperatorDAO.java
@@ -0,0 +1,4 @@
+package com.example.ex_motricite;
+
+public class OperatorDAO {
+}
