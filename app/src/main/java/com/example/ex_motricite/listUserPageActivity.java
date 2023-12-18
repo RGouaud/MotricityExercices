4257f3739bfeb6365bddad320dc2f49267bb5543
EduardoXav <eduardoxavier612@gmail.com>
2023-12-18 11:03:35 +0100

    list user page

--- app/src/main/java/com/example/ex_motricite/listUserPageActivity.java
+++ app/src/main/java/com/example/ex_motricite/listUserPageActivity.java
@@ -6,7 +6,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ToggleButton;
-import android.widget.CompoundButton;
 
 
 public class listUserPageActivity extends AppCompatActivity {
@@ -19,8 +18,8 @@ public class listUserPageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_list_user_page);
 
         buttonAddPatient = findViewById(R.id.buttonAddPatient);
-        ToggleButton toggleButton1 = findViewById(R.id.toggleButton);
-        ToggleButton toggleButton2 = findViewById(R.id.toggleButton2);
+        ToggleButton toggleButtonPatient = findViewById(R.id.buttonOperator);
+        ToggleButton toggleButtonOperator = findViewById(R.id.buttonPatient);
 
         buttonAddPatient.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,31 +29,44 @@ public class listUserPageActivity extends AppCompatActivity {
             }
         });
 
-        toggleButton1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        toggleButtonPatient.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Si le premier bouton est coché, décochez le deuxième
-                    toggleButton2.setChecked(false);
+            public void onClick(View v) {
+                // Check if the patient toggle button is clicked
+                if (toggleButtonPatient.isChecked()) {
+                    // If patient toggle button is checked, uncheck the operator toggle button
+                    toggleButtonOperator.setChecked(false);
                 } else {
-                    // Si le premier bouton est décoché, cochez le deuxième
-                    toggleButton2.setChecked(true);
+                    // If patient toggle button is unchecked
+                    if (!toggleButtonOperator.isChecked()) {
+                        // If operator toggle button is also unchecked, check the patient toggle button
+                        toggleButtonPatient.setChecked(true);
+                    } else {
+                        // If operator toggle button is checked, keep both buttons checked
+                        toggleButtonOperator.setChecked(true);
+                    }
                 }
             }
         });
 
-        toggleButton2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        toggleButtonOperator.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Si le deuxième bouton est coché, décochez le premier
-                    toggleButton1.setChecked(false);
+            public void onClick(View v) {
+                // Check if the operator toggle button is clicked
+                if (toggleButtonOperator.isChecked()) {
+                    // If operator toggle button is checked, uncheck the patient toggle button
+                    toggleButtonPatient.setChecked(false);
                 } else {
-                    // Si le premier bouton est décoché, cochez le deuxième
-                    toggleButton1.setChecked(true);
+                    // If operator toggle button is unchecked
+                    if (!toggleButtonPatient.isChecked()) {
+                        // If patient toggle button is also unchecked, check the operator toggle button
+                        toggleButtonOperator.setChecked(true);
+                    } else {
+                        // If patient toggle button is checked, keep both buttons checked
+                        toggleButtonPatient.setChecked(true);
+                    }
                 }
             }
         });
-
     }
 }
