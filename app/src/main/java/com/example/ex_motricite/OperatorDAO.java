34604e02d9642d795fde46a75f074ca4e8c5db30
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2023-12-18 10:33:55 +0100

    Finalisation des classes DAO et métiers des operateurs et des patients, démarrage des tests dans les activity

--- app/src/main/java/com/example/ex_motricite/OperatorDAO.java
+++ app/src/main/java/com/example/ex_motricite/OperatorDAO.java
@@ -1,4 +1,82 @@
 package com.example.ex_motricite;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
 public class OperatorDAO {
+    private static String base = "BDMotricity";
+    private static int version = 1;
+    private BdSQLiteOpenHelper accesBD;
+    public OperatorDAO(Context ct) {
+        accesBD = new BdSQLiteOpenHelper(ct, base, null, version);
+    }
+
+    public Operator getOperator(int idOperator){
+        Operator anOperator = null;
+        Cursor curseur;
+        curseur = accesBD.getReadableDatabase().rawQuery("select * from operator where idOperator="+idOperator+";",null);
+        if (curseur.getCount() > 0) {
+            curseur.moveToFirst();
+            anOperator = new Operator(idOperator, curseur.getString(1),curseur.getString(2));
+        }
+        return anOperator;
+    }
+
+    public void addOperator(Operator operator){
+        accesBD.getWritableDatabase().execSQL("INSERT INTO operator (name, firstName) VALUES ('" + operator.getName() + "','" + operator.getFirstName() + "');");
+        accesBD.close();
+    }
+
+    public void delOperator(Operator operator){
+        accesBD.getWritableDatabase().execSQL("delete from operator where idP="+operator.getIdOperator()+";");
+        accesBD.close();
+    }
+
+    public ArrayList<Operator> getOperators(){
+        Cursor curseur;
+        String req = "select * from operator;";
+        curseur = accesBD.getReadableDatabase().rawQuery(req,null);
+        return cursorToOperatorArrayList(curseur);
+    }
+
+    public void updateOperator(String oldName, String newName, String newFirstName) {
+        StringBuilder updateQuery = new StringBuilder("UPDATE Operator SET ");
+
+        if (!newName.isEmpty()) {
+            updateQuery.append("name = '").append(newName).append("', ");
+        }
+        if (!newFirstName.isEmpty()) {
+            updateQuery.append("firstName = '").append(newFirstName).append("', ");
+        }
+
+        // Supprimer la virgule finale si la requête n'est pas vide
+        if (updateQuery.charAt(updateQuery.length() - 1) == ' ') {
+            updateQuery.setLength(updateQuery.length() - 2); // Supprimer la virgule et l'espace
+            updateQuery.append(" ");
+            updateQuery.append("WHERE name = '").append(oldName).append("';");
+
+            accesBD.getWritableDatabase().execSQL(updateQuery.toString());
+            accesBD.close();
+        }
+    }
+
+    private ArrayList<Operator> cursorToOperatorArrayList(Cursor curseur){
+        ArrayList<Operator> listOperators = new ArrayList<Operator>();
+        long idOperator;
+        String name;
+        String firstName;
+
+        curseur.moveToFirst();
+        while (!curseur.isAfterLast()){
+            idOperator = curseur.getLong(0);
+            name = curseur.getString(1);
+            firstName = curseur.getString(2);
+            listOperators.add(new Operator(idOperator, name, firstName));
+            curseur.moveToNext();
+        }
+
+        return listOperators;
+    }
 }
--- app/src/main/java/com/example/ex_motricite/PatientDAO.java
+++ app/src/main/java/com/example/ex_motricite/PatientDAO.java
@@ -1,7 +1,95 @@
 package com.example.ex_motricite;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
 public class PatientDAO {
-    private static String base = "BDApppliPlat";
+    private static String base = "BDMotricity";
     private static int version = 1;
     private BdSQLiteOpenHelper accesBD;
+    public PatientDAO(Context ct) {
+        accesBD = new BdSQLiteOpenHelper(ct, base, null, version);
+    }
+
+    public Patient getPatient(int idPatient){
+        Patient lePatient = null;
+        Cursor curseur;
+        curseur = accesBD.getReadableDatabase().rawQuery("select * from patient where idPatient="+idPatient+";",null);
+        if (curseur.getCount() > 0) {
+            curseur.moveToFirst();
+            lePatient = new Patient(idPatient, curseur.getString(1),curseur.getString(2), curseur.getString(3),curseur.getString(4));
+        }
+        return lePatient;
+    }
+
+    public void addPatient(Patient patient){
+        accesBD.getWritableDatabase().execSQL("INSERT INTO patient (name, firstName, birthDate, remarks) VALUES ('" + patient.getName() + "','" + patient.getFirstName() + "','" + patient.getBirthDate() + "','" + patient.getRemarks() + "');");
+        accesBD.close();
+    }
+
+    public void delPatient(Patient patient){
+        accesBD.getWritableDatabase().execSQL("delete from plat where idP="+patient.getIdPatient()+";");
+        accesBD.close();
+    }
+
+    public ArrayList<Patient> getPlats(){
+        Cursor curseur;
+        String req = "select * from patient;";
+        curseur = accesBD.getReadableDatabase().rawQuery(req,null);
+        return cursorToPatientArrayList(curseur);
+    }
+
+    public void updatePatient(String oldName, String newName, String newFirstName, String newBirthDate, String newRemarks) {
+        StringBuilder updateQuery = new StringBuilder("UPDATE Patient SET ");
+
+        if (!newName.isEmpty()) {
+            updateQuery.append("name = '").append(newName).append("', ");
+        }
+        if (!newFirstName.isEmpty()) {
+            updateQuery.append("firstName = '").append(newFirstName).append("', ");
+        }
+        if (!newBirthDate.isEmpty()) {
+            updateQuery.append("birthDate = '").append(newBirthDate).append("', ");
+        }
+        if (!newRemarks.isEmpty()) {
+            updateQuery.append("remarks = '").append(newRemarks).append("', ");
+        }
+
+        // Supprimer la virgule finale si la requête n'est pas vide
+        if (updateQuery.charAt(updateQuery.length() - 1) == ' ') {
+            updateQuery.setLength(updateQuery.length() - 2); // Supprimer la virgule et l'espace
+            updateQuery.append(" ");
+            updateQuery.append("WHERE name = '").append(oldName).append("';");
+
+            accesBD.getWritableDatabase().execSQL(updateQuery.toString());
+            accesBD.close();
+        }
+    }
+
+
+
+
+    private ArrayList<Patient> cursorToPatientArrayList(Cursor curseur){
+        ArrayList<Patient> listPatients = new ArrayList<Patient>();
+        long idPatient;
+        String name;
+        String firstName;
+        String birthDate;
+        String remarks;
+
+        curseur.moveToFirst();
+        while (!curseur.isAfterLast()){
+            idPatient = curseur.getLong(0);
+            name = curseur.getString(1);
+            firstName = curseur.getString(2);
+            birthDate = curseur.getString(3);
+            remarks = curseur.getString(4);
+            listPatients.add(new Patient(idPatient, name, firstName, birthDate, remarks));
+            curseur.moveToNext();
+        }
+
+        return listPatients;
+    }
 }
