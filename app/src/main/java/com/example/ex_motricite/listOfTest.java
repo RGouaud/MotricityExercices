5c751bee8a85f02f5b07cc1bec5b5c18c3d52c81
EduardoXav <eduardoxavier612@gmail.com>
2023-12-19 11:04:29 +0100

    creation de la page de liste de tests

---
+++ app/src/main/java/com/example/ex_motricite/listOfTest.java
@@ -0,0 +1,14 @@
+package com.example.ex_motricite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listOfTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_of_test);
+    }
+}
