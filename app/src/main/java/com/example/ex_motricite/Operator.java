235932daa342996bec6b3f5639a0ff15f7729ec3
EduardoXav <eduardoxavier612@gmail.com>
2024-01-24 12:18:10 +0100

    refactor page Operator et OperatorDAO

--- app/src/main/java/com/example/ex_motricite/Operator.java
+++ app/src/main/java/com/example/ex_motricite/Operator.java
@@ -1,5 +1,7 @@
 package com.example.ex_motricite;
 
+import androidx.annotation.NonNull;
+
 public class Operator extends Actor {
 
     public Operator(long id, String name, String firstName){
@@ -10,6 +12,7 @@ public class Operator extends Actor {
         super(-1, name, firstName);
     }
 
+    @NonNull
     public String toString(){
         return "Id of Patient : " + this.getId() + ", name of patient : " + this.getName() + ", firstname of patient : " + this.getFirstName();
     }
