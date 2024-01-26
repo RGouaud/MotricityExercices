c86ec437c142bcb9cea930876b9bf9f1762bcfbe
EduardoXav <eduardoxavier612@gmail.com>
2024-01-26 10:32:55 +0100

    javadoc Operator.java, OperatorDAO.java

--- app/src/main/java/com/example/ex_motricite/OperatorDAO.java
+++ app/src/main/java/com/example/ex_motricite/OperatorDAO.java
@@ -6,14 +6,39 @@ import android.database.Cursor;
 
 import java.util.ArrayList;
 
+/**
+ * The {@code OperatorDAO} class represents a DAO (Data Access Object) for operators.
+ *
+ * <p>
+ * This class is used to retrieve operator data from a database. It provides methods to retrieve, add, delete, and update
+ * operator data in the database.
+ * </p>
+ *
+ * <p>
+ *     Author: Ferreria
+ *     Version: 1.0
+ * </p>
+ */
 public class OperatorDAO {
     private static final String BASE = "BDMotricity";
     private static final int VERSION = 1;
     private final BdSQLiteOpenHelper accesBD;
+
+    /**
+     * Constructor for OperatorDAO class.
+     *
+     * @param ct Android application context.
+     */
     public OperatorDAO(Context ct) {
         accesBD = new BdSQLiteOpenHelper(ct, BASE, null, VERSION);
     }
 
+    /**
+     * Returns an operator based on its ID.
+     *
+     * @param idOperator ID of the operator.
+     * @return Operator with the specified ID.
+     */
     @SuppressLint("Recycle")
     public Operator getOperator(long idOperator){
         Operator anOperator = null;
@@ -26,16 +51,31 @@ public class OperatorDAO {
         return anOperator;
     }
 
+    /**
+     * Adds an operator to the database.
+     *
+     * @param operator Operator to be added.
+     */
     public void addOperator(Operator operator){
         accesBD.getWritableDatabase().execSQL("INSERT INTO operator (name, firstName) VALUES ('" + operator.getName() + "','" + operator.getFirstName() + "');");
         accesBD.close();
     }
 
+    /**
+     * Deletes an operator from the database.
+     *
+     * @param operator Operator to be deleted.
+     */
     public void delOperator(Operator operator){
         accesBD.getWritableDatabase().execSQL("delete from operator where idOperator="+operator.getId()+";");
         accesBD.close();
     }
 
+    /**
+     * Returns a list of all operators in the database.
+     *
+     * @return List of all operators in the database.
+     */
     public ArrayList<Operator> getOperators(){
         Cursor curseur;
         String req = "select * from operator;";
@@ -43,6 +83,11 @@ public class OperatorDAO {
         return cursorToOperatorArrayList(curseur);
     }
 
+    /**
+     * Updates an operator in the database.
+     *
+     * @param operator Operator to be updated.
+     */
     public void updateOperator(Operator operator) {
         accesBD.getWritableDatabase().execSQL("UPDATE operator SET " +
                 "name='" + operator.getName() + "', " +
@@ -51,6 +96,12 @@ public class OperatorDAO {
         accesBD.close();
     }
 
+    /**
+     * Returns a list of operators based on a cursor.
+     *
+     * @param curseur Cursor containing operator data.
+     * @return List of operators based on the cursor.
+     */
     private ArrayList<Operator> cursorToOperatorArrayList(Cursor curseur){
         ArrayList<Operator> listOperators = new ArrayList<>();
         long idOperator;
