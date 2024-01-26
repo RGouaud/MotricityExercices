6e16e09c36d5478f774eba731f71f8605c69da35
rgouaud <r.gouaud@gmail.com>
2024-01-26 11:43:10 +0100

    ajout d'une classe ConstIntent contenant les constantes pour les parametres d'intent

---
+++ app/src/main/java/com/example/ex_motricite/ConstIntent.java
@@ -0,0 +1,26 @@
+package com.example.ex_motricite;
+
+public class ConstIntent {
+    // fields
+    public static final String USER_ID = "USER_ID";
+    public static final String FILE_PATH = "FILE_PATH";
+    public static final String DISTANCE_BETWEEN_POINTS = "DISTANCE_BETWEEN_POINTS";
+    public static final String TIME = "TIME";
+    public static final String PATIENT = "PATIENT";
+    public static final String OPERATOR = "OPERATOR";
+    public static final String INTERVAL = "INTERVAL";
+
+
+    public static final String EXERCISE = "EXERCISE";
+    public static final String USER_TYPE = "USER_TYPE";
+    public static final String CRUD_TYPE = "CRUD_TYPE";
+    // values
+    public static final String EXERCISE_STATIC = "static";
+    public static final String EXERCISE_RHYTHM = "rhythm";
+    public static final String USER_TYPE_PATIENT = "patient";
+    public static final String USER_TYPE_OPERATOR = "operator";
+    public static final String CRUD_TYPE_CREATE = "create";
+    public static final String CRUD_TYPE_UPDATE = "update";
+    public static final String CRUD_TYPE_READ = "read";
+
+}
