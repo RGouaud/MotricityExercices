b18d3024dccfb4c6af34dee52d2453d77ec0b743
rgouaud <r.gouaud@gmail.com>
2024-01-27 14:20:15 +0100

    ajout des tests pour les constructeurs

--- app/src/test/java/com/example/ex_motricite/OperatorTest.java
+++ app/src/test/java/com/example/ex_motricite/OperatorTest.java
@@ -5,4 +5,31 @@ import static org.junit.Assert.*;
 
 public class OperatorTest {
 
+    // CONSTRUCTORS
+    @Test
+    public void testConstructor2Params(){
+        //GIVEN
+        String name = "Doe";
+        String firstName = "John";
+        //WHEN
+        Operator operator = new Operator(name, firstName);
+        //THEN
+        assertEquals(operator.name, name);
+        assertEquals(operator.firstName, firstName);
+        assertEquals(-1, operator.id);
+    }
+
+    @Test
+    public void testConstructor3Params(){
+        //GIVEN
+        long id = 1;
+        String name = "Doe";
+        String firstName = "John";
+        //WHEN
+        Operator operator = new Operator(id, name, firstName);
+        //THEN
+        assertEquals(operator.id, id);
+        assertEquals(operator.name, name);
+        assertEquals(operator.firstName, firstName);
+    }
 }
