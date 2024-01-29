3621966cf3379527d3c2466852f87d4a6d66a27f
EduardoXav <eduardoxavier612@gmail.com>
2024-01-29 10:02:07 +0100

    setup gradle pour le javadoc

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation(fileTree(mapOf("dir" to "/Users/eduardo/Library/Android/sdk/platforms/android-34", "include" to listOf("*.aar", "*.jar"))))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
