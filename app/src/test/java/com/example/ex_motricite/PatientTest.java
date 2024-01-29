527a5cb2d064beb0b9955c6bdd76a433020fce34
rgouaud <r.gouaud@gmail.com>
2024-01-29 10:41:27 +0100

    ajout des tests pour les constructeurs avec erreur dans la date

--- app/src/test/java/com/example/ex_motricite/PatientTest.java
+++ app/src/test/java/com/example/ex_motricite/PatientTest.java
@@ -32,4 +32,41 @@ public class PatientTest {
         // THEN
         assertEquals("Id of Patient : -1, name of patient : " + name + ", firstname of patient : " + firstName + ", birthdate of patient : " + birthDate + ", remarks : " + remarks, patient.toString());
     }
+
+    @Test
+    public void testPatientWhenDateIsNotCorrectWithID(){
+        // GIVEN
+        long id = 1;
+        String name = "Doe";
+        String firstName = "John";
+        String birthDate = "01/011/200";
+        String remarks = "No remarks";
+        // WHEN
+        try{
+            Patient patient = new Patient(id, name, firstName, birthDate, remarks);
+            fail("Should throw IllegalArgumentException");
+        } catch (IllegalArgumentException e){
+
+        // THEN
+            assertEquals("Birthdate is not valid", e.getMessage());
+        }
+    }
+
+    @Test
+    public void testPatientWhenDateIsNotCorrectWithoutID(){
+        // GIVEN
+        String name = "Doe";
+        String firstName = "John";
+        String birthDate = "01/011/200";
+        String remarks = "No remarks";
+        // WHEN
+        try{
+            Patient patient = new Patient(name, firstName, birthDate, remarks);
+            fail("Should throw IllegalArgumentException");
+        } catch (IllegalArgumentException e){
+
+            // THEN
+            assertEquals("Birthdate is not valid", e.getMessage());
+        }
+    }
 }
