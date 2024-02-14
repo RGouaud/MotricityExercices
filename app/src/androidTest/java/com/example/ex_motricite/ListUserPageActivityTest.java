ff121b69fa56428a34427e55779ac1c3c0e65786
alexandre.ferreira <alexandre.ferreira@hupi.fr>
2024-02-14 09:53:16 +0100

    Création de la classe de test ListUserPageActivityTest, écriture de tous les scénarios dans la classe. Début du codage.

---
+++ app/src/androidTest/java/com/example/ex_motricite/ListUserPageActivityTest.java
@@ -0,0 +1,83 @@
+package com.example.ex_motricite;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+class ListUserPageActivityTest {
+
+    @Test
+    public void test_add_button_when_patient_is_selected(){
+
+    }
+
+    @Test
+    public void test_add_button_when_operator_is_selected(){
+
+    }
+
+
+    @Test
+    public void test_if_operators_are_displayed(){
+
+    }
+
+    @Test
+    public void test_if_patients_are_displayed(){
+
+    }
+
+    @Test
+    public void test_if_operator_edit_button_navigate_to_edit_page(){
+
+    }
+
+    @Test
+    public void test_if_patient_edit_butoon_navigate_to_edit_page(){
+
+    }
+
+    @Test
+    public void test_if_operator_layout_navigate_to_details(){
+
+    }
+
+    @Test
+    public void test_if_patient_layout_navigate_to_details(){
+
+    }
+
+    @Test
+    public void test_if_orientation_lock_is_set_to_portrait(){
+
+    }
+
+    @Test
+    public void test_if_buttons_are_enabled_or_disabled_based_on_user_selection(){
+
+    }
+
+    @Test
+    public void test_if_data_is_correctly_refreshed_after_adding_or_editing_an_operator(){
+
+    }
+
+    @Test
+    public void test_if_data_is_correctly_refreshed_after_adding_or_editing_a_patient(){
+
+    }
+
+    @Test
+    public void test_if_data_is_correctly_deleted_after_deleting_an_operator(){
+
+    }
+
+    @Test
+    public void test_if_data_is_correctly_deleted_after_deleting_a_patient(){
+
+    }
+}
