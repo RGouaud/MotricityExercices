5db622bd38afaf74e738f134c6884e8b50289bae
Maxime Segot <max.segot@gmail.com>
2024-02-20 00:38:23 +0100

    mauvaise branche

--- app/src/main/java/com/example/ex_motricite/TestsFragment.java
+++ app/src/main/java/com/example/ex_motricite/TestsFragment.java
@@ -10,50 +10,12 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link TestsFragment#newInstance} factory method to
+ * Use the {@link TestsFragment} factory method to
  * create an instance of this fragment.
  */
 public class TestsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TestsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TestsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TestsFragment newInstance(String param1, String param2) {
-        TestsFragment fragment = new TestsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
